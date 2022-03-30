d25140c1cea4a8705f49648c86ecab2c61ab545f
narwhal04 <q.quek7@gmail.com>
2022-03-30 23:03:27 +0800

    functioning code

--- ifElseIfStatements.go
+++ ifElseIfStatements.go
@@ -1,6 +1,33 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 	//Insert your code here
 	//Hint if a:= ?? ; condition {  }
+
+	var testNum int
+
+	fmt.Printf("Input Number:")
+	fmt.Scan(&testNum)
+
+	if testNum%2 == 0 {
+		fmt.Println(testNum, " is an even number")
+		if testNum <= 9 {
+			fmt.Printf("%v is single digit\n", testNum)
+		} else {
+			fmt.Printf("%v is double digit\n", testNum)
+		}
+	} else {
+		fmt.Printf("%v is an odd number\n", testNum)
+		if testNum <= 9 {
+			fmt.Printf("%v is single digit\n", testNum)
+		} else {
+			fmt.Printf("%v is double digit\n", testNum)
+		}
+
+	}
+
 }
